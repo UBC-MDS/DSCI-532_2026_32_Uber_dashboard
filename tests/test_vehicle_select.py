b78d093937a8038d0c79e94fb4782dbe12d9e50b
Alan Liu <alnliu2000@gmail.com>
2026-03-16 10:46:55 -0700

    Simplify test_vehicle_select.py and test the right behavior

--- tests/test_vehicle_select.py
+++ tests/test_vehicle_select.py
@@ -1,31 +1,22 @@
-import pytest
+from shiny.pytest import create_app_fixture
 from shiny.playwright import controller
 from playwright.sync_api import Page
 from shiny.run import ShinyAppProc
 
+app = create_app_fixture("../src/app.py")
 
-def test_basic_app(page: Page, local_app: ShinyAppProc) -> None:
-    page.goto(local_app.url)
+def test_basic_app(page: Page, app: ShinyAppProc) -> None:
+    page.goto(app.url)
 
     total_bookings = controller.OutputText(page, "total_bookings")
     selectize = controller.InputSelectize(page, "vehicle_type")
-    reset_btn = controller.InputActionButton(page, "action_button")
 
     # initial state
     selectize.expect_selected([])
 
     # select vehicle
-    selectize.set(["Auto", "Bike"])
-    selectize.expect_selected(["Auto", "Bike"])
-
-    page.wait_for_timeout(30000)  # wait for reactive update
+    selectize.set("Auto")
+    selectize.expect_selected(["Auto"])
 
     # total bookings for Auto and Bike should be 37K
-    total_bookings.expect_value("37K")
-
-    # reset filters
-    reset_btn.click()
-    page.wait_for_load_state("networkidle")
-
-    # expect cleared
-    selectize.expect_selected([])
+    total_bookings.expect_value("37K", timeout=20000)
